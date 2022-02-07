8efacf56927ac123a9e3fdb3918fd583436f271b
AyushMathpal <92756482+AyushMathpal@users.noreply.github.com>
2022-02-07 15:01:13 -0800

    Create Celsius to Fahrenheit conversion.cpp

---
+++ Operators, Conditional Statements and Loops/Celsius to Fahrenheit conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    float tf;
+    float factor = (float)9 / (float)5;
+    tf=tc*factor+32;
+    cout<<tf;
+    return 0;
+}
